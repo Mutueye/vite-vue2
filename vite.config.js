257e5694f5630bf75c15850d14c3a9a89c70ba50
Mutueye <mutuyueniao@gmail.com>
2021-06-28 11:59:00 +0800

    vite 配置兼容性插件

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,20 @@
 import path from 'path';
 import { defineConfig } from 'vite';
-const { createVuePlugin } = require('vite-plugin-vue2');
+import { createVuePlugin } from 'vite-plugin-vue2';
+import legacy from '@vitejs/plugin-legacy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   define: {
     'process.env': process.env,
   },
-  plugins: [createVuePlugin()],
+  plugins: [
+    createVuePlugin(),
+    legacy({
+      targets: ['defaults', 'ie >= 11'],
+      additionalLegacyPolyfills: ['regenerator-runtime/runtime'],
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
